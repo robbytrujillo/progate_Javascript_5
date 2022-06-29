85940bd4dd484fdf9ab017a9677ca76cd04825da
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2022-06-29 09:31:30 +0700

    Progate Latihan JS ES6 V5

---
+++ page3/script.js
@@ -0,0 +1,11 @@
+// Hapus code dibawah
+//import Dog from "./dog";
+
+//const dog = new Dog("Leo", 4, "Chihuahua");
+// Hapus code diatas
+
+
+// Import constant dog
+import dog from "./dogData";
+
+dog.info();
